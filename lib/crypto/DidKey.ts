34e93eeaeb05ed4c0422d375bfcebf60cf6b2576
Ronny Bjones <ronnybj@microsoft.com>
2019-03-19 10:18:22 -0700

    Switch from promises to async API

--- lib/crypto/DidKey.ts
+++ lib/crypto/DidKey.ts
@@ -125,94 +125,65 @@ export default class DidKey {
   /**
    * Gets the key in jwk format.
    */
-  public getJwkKey (keyExport: KeyExport): Promise<any> {
+  public async getJwkKey (keyExport: KeyExport): Promise<any> {
     // check if key is already cached
     let keyId = this.getKeyIdentifier(this.keyType, this.keyUse, keyExport);
     let jwkKey = this.getJwkKeyFromCache(keyId);
     if (jwkKey) {
       // Return the key if it already exists
-      return new Promise((resolve) => {
-        resolve(jwkKey);
-      });
+      return jwkKey;
     }
 
     // Get the key or generate the key if needed
-    return this.getOrGenerateKey()
-    .then((keyObject: KeyObject) => {
-      // Cache the key object
-      this.cacheKeyObject(keyId, keyObject);
-      return keyObject;
-    })
-    .then((keyObject: KeyObject) => {
-      // export to jwk format
-      return this.getJwkKeyFromKeyObject(keyExport, keyObject);
-    })
-    .then((jwk: any) => {
-      // Save jwk format
-      this.cacheJwkKey(keyId, jwk);
-      return jwk;
-    })
-    .then((jwk: any) => {
-      // Check to save public key
-      if (this.isKeyPair && keyExport === KeyExport.Private) {
-        // Save only public key
-        let jwkPublic: any = {};
-        jwkPublic.kty = jwk.kty;
-        jwkPublic.use = jwk.use;
-        jwkPublic.key_ops = jwk.key_ops;
-        if (this.keyType === KeyType.RSA) {
-          jwkPublic.e = jwk.e;
-          jwkPublic.n = jwk.n;
-        } else {
-          jwkPublic.crv = jwk.crv;
-          jwkPublic.x = jwk.x;
-          jwkPublic.y = jwk.y;
-        }
+    let keyObject: KeyObject = await this.getOrGenerateKey();
+    // Cache the key object
+    this.cacheKeyObject(keyId, keyObject);
+    // export to jwk format
+    let jwk: any = await this.getJwkKeyFromKeyObject(keyExport, keyObject);
 
-        // Save public key
-        let keyIdPublick = this.getKeyIdentifier(this.keyType, this.keyUse, KeyExport.Public);
-        this.cacheJwkKey(keyIdPublick, jwkPublic);
-        return this._crypto.subtle.importKey('jwk', jwkPublic, this._algorithm, this._exportable, this.setKeyUsage())
-        .then((keyObject: any) => {
-          this.cacheKeyObject(keyIdPublick, new KeyObject(this.keyType, keyObject));
-          return jwk;
-        }).catch((err: any) => {
-          console.error(err);
-          throw new Error(`DidKey:setOctKey->importKey threw ${err}`);
-        });
+    // Save jwk format
+    this.cacheJwkKey(keyId, jwk);
+
+    // Check to save public key
+    if (this.isKeyPair && keyExport === KeyExport.Private) {
+      // Save only public key
+      let jwkPublic: any = {};
+      jwkPublic.kty = jwk.kty;
+      jwkPublic.use = jwk.use;
+      jwkPublic.key_ops = jwk.key_ops;
+      if (this.keyType === KeyType.RSA) {
+        jwkPublic.e = jwk.e;
+        jwkPublic.n = jwk.n;
+      } else {
+        jwkPublic.crv = jwk.crv;
+        jwkPublic.x = jwk.x;
+        jwkPublic.y = jwk.y;
       }
-      return jwk;
-    })
-    .catch((err) => {
-      throw new Error(`getJwkKey had error: ${err}'`);
-    });
+
+      // Save public key
+      let keyIdPublick = this.getKeyIdentifier(this.keyType, this.keyUse, KeyExport.Public);
+      this.cacheJwkKey(keyIdPublick, jwkPublic);
+      let keyObject: any = await this._crypto.subtle.importKey('jwk', jwkPublic, this._algorithm, this._exportable, this.setKeyUsage());
+      this.cacheKeyObject(keyIdPublick, new KeyObject(this.keyType, keyObject));
+    }
+    return jwk;
   }
 
   /**
    * Sign the data with the current key
    * @param data  Data to be signed with the current key
    */
-  public sign (data: Buffer): Promise<ArrayBuffer> {
+  public async sign (data: Buffer): Promise<ArrayBuffer> {
     let keyExport = this.isKeyPair ? KeyExport.Private : KeyExport.Secret;
     let keyId = this.getKeyIdentifier(this.keyType, this.keyUse, keyExport);
     // console.log(`Sign data: ${base64url(data)} with ${keyId}`);
-    return this.getJwkKey(keyExport)
-    .then(() => {
-      let keyObject = this.getKeyObject(keyId);
-      if (keyObject) {
-        return this._crypto.subtle.sign(this._algorithm, this.isKeyPair ? (keyObject as any).privateKey : (keyObject as any).secretKey, data)
-        .catch((err: any) => {
-          console.error(err);
-          throw new Error(`DidKey:sign->Signature failed ${err}`);
-        });
-      } else {
-        throw new Error(`No private key for signature: ${keyId}`);
-      }
-    })
-      .catch((err: any) => {
-        console.error(err);
-        throw new Error(`DidKey:getJwkKey->Failed ${err}`);
-      });
+    await this.getJwkKey(keyExport);
+    let keyObject = this.getKeyObject(keyId);
+    if (keyObject) {
+      return this._crypto.subtle.sign(this._algorithm, this.isKeyPair ? (keyObject as any).privateKey : (keyObject as any).secretKey, data);
+    } else {
+      throw new Error(`No private key for signature: ${keyId}`);
+    }
   }
 
   /**
@@ -220,28 +191,13 @@ export default class DidKey {
    * @param data  The data signed with the current key
    * @param signature  The signature on the data
    */
-  public verify (data: Buffer, signature: ArrayBuffer): Promise<boolean> {
+  public async verify (data: Buffer, signature: ArrayBuffer): Promise<boolean> {
     // console.log(`Verify data: ${base64url(data)}`);
     let keyExport = this.isKeyPair ? KeyExport.Public : KeyExport.Secret;
-    return this.getJwkKey(keyExport)
-    .then((jwk) => {
-      jwk.key_ops = ['verify'];
-      return this._crypto.subtle.importKey('jwk', jwk, this._algorithm, this._exportable, this.setKeyUsage())
-      .then((keyObject: any) => {
-        return this._crypto.subtle.verify(this._algorithm, keyObject, signature, data)
-        .catch((err: any) => {
-          console.error(err);
-          throw new Error(`DidKey:Verify->Signature failed ${err}`);
-        });
-      }).catch((err: any) => {
-        console.error(err);
-        throw new Error(`DidKey:verify->importKey threw ${err}`);
-      });
-    })
-    .catch((err: any) => {
-      console.error(err);
-      throw new Error(`DidKey:getJwkKey->Failed ${err}`);
-    });
+    let jwk = await this.getJwkKey(keyExport);
+    jwk.key_ops = ['verify'];
+    let keyObject: any = await this._crypto.subtle.importKey('jwk', jwk, this._algorithm, this._exportable, this.setKeyUsage());
+    return this._crypto.subtle.verify(this._algorithm, keyObject, signature, data);
   }
 
   /**
@@ -250,53 +206,38 @@ export default class DidKey {
    * @param did  The owner DID
    * @param peerId  The representation of the peer
    */
-  public generatePairwise (seed: Buffer, did: string, peerId: string): Promise<DidKey> {
-    let pairwiseKey: DidKey;
-    return this.generateDidMasterKey(seed, did). then((didMasterKey: MasterKey) => {
-      let pairwise: DidKey | undefined = this._didPairwiseKeys.get(this.mapDidPairwiseKeys(peerId));
-      if (pairwise) {
-        return new Promise<DidKey>((resolve) => {
-          resolve(pairwise);
-        }).catch((err: any) => {
-          console.error(err);
-          throw new Error(`DidKey:generatePairwise->generatePairwise threw ${err}`);
-        });
-      }
+  public async generatePairwise (seed: Buffer, did: string, peerId: string): Promise<DidKey> {
+    let pairwiseDidKey: DidKey;
+    let didMasterKey: MasterKey = await this.generateDidMasterKey(seed, did);
+    let pairwise: DidKey | undefined = this._didPairwiseKeys.get(this.mapDidPairwiseKeys(did, peerId));
+    if (pairwise) {
+      return pairwise;
+    }
 
-      switch (this._keyType) {
-        case KeyType.EC:
-        case KeyType.RSA:
+    switch (this._keyType) {
+      case KeyType.EC:
+      case KeyType.RSA:
 
-          // Generate new pairwise key
-          const pairwiseKey: PairwiseKey = new PairwiseKey(did, peerId);
-          return pairwiseKey.generate(didMasterKey.key, this._crypto, this._algorithm, this._keyType, this._keyUse, this._exportable)
-          .then((pairwiseDidKey: DidKey) => {
-            // Cache pairwise key
-            this._didPairwiseKeys.set(this.mapDidPairwiseKeys(peerId), pairwiseDidKey);
-            return pairwiseDidKey;
-          })
-          .catch((err: any) => {
-            console.error(err);
-            throw new Error(`DidKey:generatePairwise->generate threw ${err}`);
-          });
+        // Generate new pairwise key
+        const pairwiseKey: PairwiseKey = new PairwiseKey(did, peerId);
+        pairwiseDidKey = await pairwiseKey.generate(didMasterKey.key, this._crypto, this._algorithm, this._keyType, this._keyUse, this._exportable);
 
-        default:
-          throw new Error(`Pairwise key for type '${this._keyType}' is not supported.`);
-      }
-    })
-    .then((pairwise: DidKey) => {
-      // Store private and public key.
-      pairwiseKey = pairwise;
-      return pairwise.getJwkKey(KeyExport.Private);
-    })
-      .then(() => {
-        return pairwiseKey;
-      });
+        // Cache pairwise key
+        this._didPairwiseKeys.set(this.mapDidPairwiseKeys(did, peerId), pairwiseDidKey);
+        break;
+
+      default:
+        throw new Error(`Pairwise key for type '${this._keyType}' is not supported.`);
+    }
+    // Store private and public key.
+    await pairwiseDidKey.getJwkKey(KeyExport.Private);
+    return pairwiseDidKey;
   }
 
-  private mapDidPairwiseKeys (peerId: string): string {
+  // Generate a unique key id for storage of pairwise keys
+  private mapDidPairwiseKeys (did: string, peerId: string): string {
     // TODO add key use if we want different keys for signing and encryption
-    return `${this._keyType}_${peerId}`;
+    return `${this._keyType}_${did}_${peerId}`;
   }
 
   // True if the key is a key pair
@@ -321,7 +262,7 @@ export default class DidKey {
    * @param did  The owner DID
    * @param peerId  The representation of the peer
    */
-  private generateDidMasterKey (seed: Buffer, did: string): Promise<MasterKey> {
+  private async generateDidMasterKey (seed: Buffer, did: string): Promise<MasterKey> {
     let mk: MasterKey | undefined = undefined;
 
     // Check if key was already generated
@@ -333,28 +274,16 @@ export default class DidKey {
     });
 
     if (mk) {
-      return new Promise((resolve) => {
-        resolve(mk);
-      });
+      return mk;
     }
 
     let alg = { name: 'hmac', hash: 'SHA-512' };
     let signKey: DidKey = new DidKey(this._crypto, alg, KeyType.Oct, KeyUse.Signature, seed);
-    return signKey.getJwkKey(KeyExport.Secret)
-    .then(() => {
-      return signKey.sign(Buffer.from(did))
-      .then((signature: ArrayBuffer) => {
-        mk = new MasterKey(did, Buffer.from(signature));
-        this._didMasterKeys.push(mk);
-        return mk;
-      }).catch((err: any) => {
-        console.error(err);
-        throw new Error(`DidKey:generateDidMasterKey->sign threw ${err}`);
-      });
-    }).catch((err: any) => {
-      console.error(err);
-      throw new Error(`DidKey:generateDidMasterKey->get jwkKey threw ${err}`);
-    });
+    await signKey.getJwkKey(KeyExport.Secret);
+    let signature: ArrayBuffer = await signKey.sign(Buffer.from(did));
+    mk = new MasterKey(did, Buffer.from(signature));
+    this._didMasterKeys.push(mk);
+    return mk;
   }
 
   // Set keyUsage
@@ -375,44 +304,31 @@ export default class DidKey {
   }
 
   // Transform the KeyObject into a JWK key
-  private getJwkKeyFromKeyObject (keyExport: KeyExport, keyObject: KeyObject): Promise<any> {
+  private async getJwkKeyFromKeyObject (keyExport: KeyExport, keyObject: KeyObject): Promise<any> {
     if (!keyObject) {
       throw new Error('keyObject argument in getJwkKey cannot be null');
     }
 
     switch (this._keyType) {
       case KeyType.Oct:
-        return this.getOctJwkKey(keyObject)
-        .catch((err: any) => {
-          console.error(err);
-          throw new Error(`DidKey:getJwkKey->getOctJwkKey threw ${err}`);
-        });
+        return this.getOctJwkKey(keyObject);
 
       case KeyType.RSA:
       case KeyType.EC:
-        return this.getKeyPairJwkKey(keyExport, keyObject)
-        .catch((err: any) => {
-          console.error(err);
-          throw new Error(`DidKey:getJwkKey->getKeyPairJwkKey threw ${err}`);
-        });
+        return this.getKeyPairJwkKey(keyExport, keyObject);
     }
 
     throw new Error(`DidKey:getJwkKey->${this._keyType} is not supported`);
   }
 
   // Transform the oct KeyObject into a JWK key.
-  private getOctJwkKey (keyObject: KeyObject): Promise<any> {
-    return this._crypto.subtle.exportKey('jwk', keyObject.secretKey)
-    .then((jwk: any) => {
-      return jwk;
-    }).catch((err: any) => {
-      console.error(err);
-      throw new Error(`DidKey:getOctJwkKey->exportKey threw ${err}`);
-    });
+  private async getOctJwkKey (keyObject: KeyObject): Promise<any> {
+    let jwk: any = await this._crypto.subtle.exportKey('jwk', keyObject.secretKey);
+    return jwk;
   }
 
   // Transform the key pair KeyObject into a JWK key.
-  private getKeyPairJwkKey (keyExport: KeyExport, keyObject: KeyObject): Promise<any> {
+  private async getKeyPairJwkKey (keyExport: KeyExport, keyObject: KeyObject): Promise<any> {
     let nativeKey = undefined;
     switch (keyExport) {
       case KeyExport.Private:
@@ -423,13 +339,8 @@ export default class DidKey {
         break;
     }
 
-    return this._crypto.subtle.exportKey('jwk', nativeKey)
-    .then((jwk: any) => {
-      return jwk;
-    }).catch((err: any) => {
-      console.error(err);
-      throw new Error(`DidKey:getOctJwkKey->exportKey threw ${err}`);
-    });
+    let jwk: any = await this._crypto.subtle.exportKey('jwk', nativeKey);
+    return jwk;
   }
 
   private getKeyIdentifier (keyType: KeyType, keyUse: KeyUse, keyExport: KeyExport): string {
@@ -460,7 +371,7 @@ export default class DidKey {
 
   // Get the key or generate the key if needed
   // Return a keyObject
-  private getOrGenerateKey (): Promise<KeyObject> {
+  private async getOrGenerateKey (): Promise<KeyObject> {
     if (this._rawKey === null) {
       // indicate key is generated and raw key was not set by caller
       this._rawKey = undefined;
@@ -479,7 +390,7 @@ export default class DidKey {
   }
 
   // Generate KeyObject from raw key
-  private setFromRawKey (key: any): Promise<KeyObject> {
+  private async setFromRawKey (key: any): Promise<KeyObject> {
     if (!key) {
       throw new Error('Key must be defined');
     }
@@ -495,34 +406,19 @@ export default class DidKey {
       jwkKey = key;
     }
 
-    return this._crypto.subtle.importKey('jwk', jwkKey, this._algorithm, this._exportable, this.setKeyUsage())
-    .then((keyObject: any) => {
-      return new KeyObject(this.keyType, keyObject);
-    }).catch((err: any) => {
-      console.error(err);
-      throw new Error(`DidKey:setOctKey->importKey threw ${err}`);
-    });
+    let keyObject: KeyObject = await this._crypto.subtle.importKey('jwk', jwkKey, this._algorithm, this._exportable, this.setKeyUsage());
+    return new KeyObject(this.keyType, keyObject);
   }
 
   // Generate an oct key and return a key object
-  private generateOctKey (): Promise<KeyObject> {
-    return this._crypto.subtle.generateKey(this._algorithm, this._exportable, this.setKeyUsage())
-    .then((keyObject: any) => {
-      return new KeyObject(this.keyType, keyObject);
-    }).catch((err: any) => {
-      console.error(err);
-      throw new Error(`DidKey:generateOctKey->generateKey threw ${err}`);
-    });
+  private async generateOctKey (): Promise<KeyObject> {
+    let keyObject: KeyObject = await this._crypto.subtle.generateKey(this._algorithm, this._exportable, this.setKeyUsage());
+    return new KeyObject(this.keyType, keyObject);
   }
 
   // Generate a key pair and return a key object
-  private generateKeyPair (): Promise<KeyObject> {
-    return this._crypto.subtle.generateKey(this._algorithm, this._exportable, this.setKeyUsage())
-    .then((keyObject: any) => {
-      return new KeyObject(this.keyType, keyObject);
-    }).catch((err: any) => {
-      console.error(err);
-      throw new Error(`DidKey:generateKey->generateKey threw ${err}`);
-    });
+  private async generateKeyPair (): Promise<KeyObject> {
+    let keyObject: KeyObject = await this._crypto.subtle.generateKey(this._algorithm, this._exportable, this.setKeyUsage());
+    return new KeyObject(this.keyType, keyObject);
   }
 }
--- tests/crypto/DidKeyPairwiseEC.spec.ts
+++ tests/crypto/DidKeyPairwiseEC.spec.ts
@@ -10,49 +10,54 @@ const crypto = new WebCrypto();
 describe('DidKey Pairwise keys EC', () => {
 
   describe('Test Pairwise key generation', () => {
-    it('Test P-256', (done) => {
+    it('throws', async (done) => {
+      let pwDidKey: DidKey = new DidKey(crypto, { name: 'hmac', hash: 'SHA-256' }, KeyType.Oct, KeyUse.Signature, null);
+      await pwDidKey.generatePairwise(Buffer.from('abcdefg'), 'did:test:something', 'did:test:peer')
+      .catch((err) => {
+        expect(`Pairwise key for type 'oct' is not supported.`).toBe(err.message);
+      });
+      done();
+    });
+
+    it('Test P-256', async (done) => {
       const alg = { name: 'ECDSA', namedCurve: 'P-256', hash: { name: 'SHA-256' } };
 
       // Generate the key pair
       let didKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null, true);
 
-      didKey.getJwkKey(KeyExport.Private).then((ecKey1) => {
-        expect(ecKey1).not.toBeNull();
-        expect('P-256').toBe(ecKey1.crv);
-        expect('EC').toBe(ecKey1.kty);
-        done();
-      })
-      .catch((err) => {
-        fail(`Error occured: '${err}'`);
-      });
-
+      let ecKey1 = await didKey.getJwkKey(KeyExport.Private);
+      expect(ecKey1).not.toBeNull();
+      expect('P-256').toBe(ecKey1.crv);
+      expect('EC').toBe(ecKey1.kty);
+      done();
     });
 
     let seed = Buffer.from('xprv9s21ZrQH143K3QTDL4LXw2F7HEK3wJUD2nW2nRk4stbPy6cq3jPPqjiChkVvvNKmPGJxWUtg6LnF5kejMRNNU3TGtRBeJgk33yuGBxrMPHi');
 
-    it('Check PairwiseId generation uniqueness', () => {
+    it('PairwiseId generation', async () => {
+      const alg = { name: 'ECDSA', namedCurve: 'P-256K', hash: { name: 'SHA-256' } };
+      let didKey: DidKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null);
+      let pairwiseKey1: DidKey = await didKey.generatePairwise(seed, `did:test`, 'peer');
+      // return the same
+      let pairwiseKey2: DidKey = await didKey.generatePairwise(seed, `did:test`, 'peer');
+      expect(pairwiseKey1).toBe(pairwiseKey2);
+    });
+
+    it('Check PairwiseId generation uniqueness', async () => {
       let inx: number = 0;
       let results: string[] = [];
       const alg = { name: 'ECDSA', namedCurve: 'P-256K', hash: { name: 'SHA-256' } };
       let didKey: DidKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null);
       for (inx = 0 ; inx < 1000; inx++) {
-        didKey.generatePairwise(seed, `did=${inx}`, 'peer').then((pairwiseKey: DidKey) => {
-          pairwiseKey.getJwkKey(KeyExport.Private).then((jwk) => {
-            results.push(jwk.d);
-            // console.log(`Check ${jwk.d} ${results.length}`);
-            expect(1).toBe(results.filter(element => element === jwk.d).length);
-          })
-          .catch((err) => {
-            fail(`Error occured: '${err}'`);
-          });
-        })
-        .catch((err) => {
-          fail(`Error occured: '${err}'`);
-        });
+        let pairwiseKey: DidKey = await didKey.generatePairwise(seed, `did=${inx}`, 'peer');
+        let jwk = await pairwiseKey.getJwkKey(KeyExport.Private);
+        results.push(jwk.d);
+        // console.log(`Check ${jwk.d} ${results.length}`);
+        expect(1).toBe(results.filter(element => element === jwk.d).length);
       }
     });
 
-    it('Check PairwiseId generation uniqueness with different seed', (done) => {
+    it('Check PairwiseId generation uniqueness with different seed', async (done) => {
       let inx: number = 0;
       let nrIds: number = 100;
       let ids: string[] = [];
@@ -66,68 +71,38 @@ describe('DidKey Pairwise keys EC', () => {
       let didKey: DidKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null);
       inx = 0;
       for (let pwid of ids) {
-        didKey.generatePairwise(seed, did, pwid).then((pairwiseKey: DidKey) => {
-          pairwiseKey.getJwkKey(KeyExport.Private).then((jwk) => {
-            // console.log(`{ pwid: '${pwid}', inx: ${inx++}, key: '${jwk.d}'},`);
-            pairwiseKeys.forEach((element: any) => {
-              if (element.pwid === pwid) {
-                // console.log(`Check ${element.inx}: ${element.key} == ${jwk.d}`);
-                expect(element.key).not.toBe(jwk.d);
-                expect(0).toBe(pairwiseKeys.filter((element: any) => element === jwk.d).length);
-                return;
-              }
-            });
-          })
-          .catch((err) => {
-            fail(`Error occured: '${err}'`);
-          });
-        })
-        .catch((err) => {
-          fail(`Error occured: '${err}'`);
-        });
+        let pairwiseKey: DidKey = await didKey.generatePairwise(seed, did, pwid);
+        let jwk = await pairwiseKey.getJwkKey(KeyExport.Private);
+
+        // console.log(`{ pwid: '${pwid}', inx: ${inx++}, key: '${jwk.d}'},`);
+        expect(0).toBe(pairwiseKeys.filter((element: any) =>
+          element.key === jwk.d).length);
       }
 
       done();
     });
 
-    it('Check PairwiseId generation uniqueness with different peer', (done) => {
+    it('Check PairwiseId generation uniqueness with different peer', async (done) => {
       let inx: number = 0;
       let nrIds: number = 100;
-      let ids: Promise<string>[] = [];
       for (inx = 0; inx < nrIds; inx++) {
-        // ids.push(Promise.resolve(`${inx}`));
-        ids.push(new Promise((resolve, reject) => {
-          let did: string = 'abcdef';
-          const alg = { name: 'ECDSA', namedCurve: 'P-256K', hash: { name: 'SHA-256' } };
-          let didKey: DidKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null);
-          let id = `${inx}`;
-          didKey.generatePairwise(seed, did, id).then((pairwiseKey: DidKey) => {
-            return pairwiseKey.getJwkKey(KeyExport.Private);
-          }).then((jwk) => {
-            // console.log(`{ "pwid": "${id}", "key": "${jwk.d}"},`);
-            let element = pairwiseKeys.filter((item: any) => {
-              return item.pwid === id;
-            });
+        let did: string = 'abcdef';
+        const alg = { name: 'ECDSA', namedCurve: 'P-256K', hash: { name: 'SHA-256' } };
+        let didKey: DidKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null);
+        let id = `${inx}`;
+        let pairwiseKey: DidKey = await didKey.generatePairwise(seed, did, id);
+        let jwk = await pairwiseKey.getJwkKey(KeyExport.Private);
 
-            console.log(`${id}: Check ${element[0].pwid}: ${element[0].key} == ${jwk.d}`);
-            expect(element[0].key).toBe(jwk.d);
-            expect(1).toBe(pairwiseKeys.filter((element: any) => element.key === jwk.d).length);
-            resolve();
-          }).catch((err) => {
-            fail(`Error occured: '${err}'`);
-          });
-        }));
+        console.log(`{ "pwid": "${id}", "key": "${jwk.d}"},`);
+        // console.log(`${id}: Check ${pairwiseKeys[inx].pwid}: ${pairwiseKeys[inx].key} == ${jwk.d}`);
+        expect(pairwiseKeys[inx].key).toBe(jwk.d);
+        expect(1).toBe(pairwiseKeys.filter((element: any) => element.key === jwk.d).length);
       }
 
-      Promise.all(ids).then((elements) => {
-        console.log('done');
-        done();
-      }).catch((err) => {
-        fail(`Error occured: '${err}'`);
-      });
+      done();
     });
 
-    it('Check PairwiseId generation uniqueness with different did', (done) => {
+    it('Check PairwiseId generation uniqueness with different did', async (done) => {
       let inx: number = 0;
       let nrIds: number = 100;
       let ids: string[] = [];
@@ -141,31 +116,23 @@ describe('DidKey Pairwise keys EC', () => {
       inx = 0;
       let didKey: DidKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null);
       for (let pwid of ids) {
-        didKey.generatePairwise(seed, did, pwid).then((pairwiseKey: DidKey) => {
-          pairwiseKey.getJwkKey(KeyExport.Private).then((jwk) => {
-            // console.log(`{ pwid: '${pwid}', inx: ${inx++}, key: '${jwk.d}'},`);
-            pairwiseKeys.forEach((element: any) => {
-              if (element.pwid === pwid) {
-                // console.log(`Check ${element.inx}: ${element.key} == ${jwk.d}`);
-                expect(element.key).not.toBe(jwk.d);
-                expect(0).toBe(pairwiseKeys.filter((element: any) => element === jwk.d).length);
-                return;
-              }
-            });
-          })
-          .catch((err) => {
-            fail(`Error occured: '${err}'`);
-          });
-        })
-        .catch((err) => {
-          fail(`Error occured: '${err}'`);
+        let pairwiseKey: DidKey = await didKey.generatePairwise(seed, did, pwid);
+        let jwk = await pairwiseKey.getJwkKey(KeyExport.Private);
+        // console.log(`{ pwid: '${pwid}', inx: ${inx++}, key: '${jwk.d}'},`);
+        pairwiseKeys.forEach((element: any) => {
+          if (element.pwid === pwid) {
+            // console.log(`Check ${element.inx}: ${element.key} == ${jwk.d}`);
+            expect(element.key).not.toBe(jwk.d);
+            expect(0).toBe(pairwiseKeys.filter((element: any) => element === jwk.d).length);
+            return;
+          }
         });
       }
 
       done();
     });
 
-    it('Check PairwiseId generation', (done) => {
+    it('Check PairwiseId generation', async (done) => {
       let inx: number = 0;
       let nrIds: number = 100;
       let ids: string[] = [];
@@ -178,23 +145,15 @@ describe('DidKey Pairwise keys EC', () => {
       let didKey: DidKey = new DidKey(crypto, alg, KeyType.EC, KeyUse.Signature, null);
       inx = 0;
       for (let pwid of ids) {
-        didKey.generatePairwise(seed, did, pwid).then((pairwiseKey: DidKey) => {
-          pairwiseKey.getJwkKey(KeyExport.Private).then((jwk) => {
-            // console.log(`{ pwid: '${pwid}', inx: ${inx++}, key: '${jwk.d}'},`);
-            pairwiseKeys.forEach((element: any) => {
-              if (element.pwid === pwid) {
-                // console.log(`Check ${element.inx}: ${element.key} == ${jwk.d}`);
-                expect(element.key).toBe(jwk.d);
-                return;
-              }
-            });
-          })
-          .catch((err) => {
-            fail(`Error occured: '${err}'`);
-          });
-        })
-        .catch((err) => {
-          fail(`Error occured: '${err}'`);
+        let pairwiseKey: DidKey = await didKey.generatePairwise(seed, did, pwid);
+        let jwk = await pairwiseKey.getJwkKey(KeyExport.Private);
+        // console.log(`{ pwid: '${pwid}', inx: ${inx++}, key: '${jwk.d}'},`);
+        pairwiseKeys.forEach((element: any) => {
+          if (element.pwid === pwid) {
+            // console.log(`Check ${element.inx}: ${element.key} == ${jwk.d}`);
+            expect(element.key).toBe(jwk.d);
+            return;
+          }
         });
       }
 
